2e25515d86ccdc778b59036b59bac852c410d46c
App Academy Student <student@appacademy.io>
2016-12-15 15:25:29 -0800

    Completed Intro to Callbacks W5D4

--- Desktop/W5D4/callbacks.js
+++ Desktop/W5D4/callbacks.js
@@ -42,10 +42,96 @@ class Clock {
 }
 
 
-const clock = new Clock();
+// const clock = new Clock();
 
+const readline = require('readline');
+const reader = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
 
+function addNumbers(sum, numsLeft, completionCallback) {
+  if (numsLeft > 0) {
+    reader.question("Pick a number!", function(number) {
+      sum += parseInt(number);
+      console.log(`Current sum is ${sum}.`);
+      numsLeft -= 1;
+      addNumbers(sum, numsLeft, completionCallback);
 
-
+    });
+  }
+  else {
+    reader.close();
+    completionCallback(sum);
+  }
+}
 
 // addNumbers(0, 3, sum => console.log(`Total Sum: ${sum}`));
+
+function askIfGreaterThan(el1, el2, callback) {
+  reader.question(`Is ${el1} greater than ${el2}?`, function(answer) {
+    answer === 'yes' ? callback(true) : callback(false);
+  });
+}
+
+function innerBubbleSortLoop(arr, i, madeAnySwaps, outerBubbleSortLoop) {
+  if (i < arr.length - 1) {
+    askIfGreaterThan(arr[i], arr[i + 1], (isGreaterThan) => {
+      if (isGreaterThan) {
+        let temp = arr[i + 1];
+        arr[i + 1] = arr[i];
+        arr[i] = temp;
+        madeAnySwaps = true;
+      }
+      innerBubbleSortLoop(arr, i + 1, madeAnySwaps, outerBubbleSortLoop);
+    });
+  }
+  else {
+    outerBubbleSortLoop(madeAnySwaps);
+  }
+}
+
+function absurdBubbleSort(arr, sortCompletionCallback) {
+  function outerBubbleSortLoop(madeAnySwaps) {
+    if (madeAnySwaps) {
+      innerBubbleSortLoop(arr, 0, false, outerBubbleSortLoop);
+    }
+    else {
+      sortCompletionCallback(arr);
+    }
+  }
+  outerBubbleSortLoop(true);
+}
+
+// askIfGreaterThan(5, 25, function(boolean) { console.log(boolean); });
+
+// askIfGreaterThan(5, 25, boolean => console.log(boolean));
+
+// absurdBubbleSort([3, 2, 1], function (arr) {
+//   console.log("Sorted array: " + JSON.stringify(arr));
+//   reader.close();
+// });
+
+Function.prototype.myBind = function(context) {
+  return () => this.apply(context);
+};
+
+class Lamp {
+  constructor() {
+    this.name = "a lamp";
+  }
+}
+
+const turnOn = function() {
+   console.log("Turning on " + this.name);
+};
+
+const lamp = new Lamp();
+
+// turnOn(); // should not work the way we want it to
+
+const boundTurnOn = turnOn.bind(lamp);
+const myBoundTurnOn = turnOn.myBind(lamp);
+
+// boundTurnOn(); // should say "Turning on a lamp"
+myBoundTurnOn(); // should say "Turning on a lamp"
